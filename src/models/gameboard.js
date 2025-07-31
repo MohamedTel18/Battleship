d8064b875e3e4de8628440bbba6d81effd5df147
MohamedTel18 <m_telkhoukhe@estin.dz>
2025-07-31 15:16:53 +0300

    create models

--- src/models/gameboard.js
+++ src/models/gameboard.js
@@ -1,9 +1,79 @@
+import { Ship } from './ship.js';
+
 export default class gameBoard{
     constructor(){
-        this.size = 10;
         this.ships = [];
-        this.shipPsositions = new Map();
-        this.hits = new Set();
+        this.shipPositions = new Map();
+        this.attacks = new Set();
         this.misses = new Set();
+    }
+
+    PlaceShip(ship, x, y, orientation) 
+    {
+        if(!this.IsValidPosition(x, y, orientation, ship)) {
+            return false;
+        }
+        
+        const Positions = [];
+        for(let i = 0; i < ship.length; i++) {
+            const posX = orientation === 'horizontal' ? x + i : x;
+            const posY = orientation === 'vertical' ? y + i : y;
+            this.shipPositions.set(`${posX},${posY}`, ship);
+            Positions.push(`${posX},${posY}`);
+        }
+
+        this.ships.push({
+            ship: ship,
+            positions: Positions,
+            isVertical: orientation,
+        });
+
+        return true;
+    }
+
+    IsValidPosition(x,y,orientation,ship)
+    {
+        const EndX = orientation === 'horizontal' ? x + ship.length - 1 : x;
+        const EndY = orientation === 'vertical' ? y + ship.length - 1 : y;
+
+        if(EndX >= 10 || EndY >= 10) return false;
+
+        for(let i = 0; i < ship.length; i++) {
+            const posX = orientation === 'horizontal' ? x + i : x;
+            const posY = orientation === 'vertical' ? y + i : y;
+            if(this.shipPositions.has(`${posX},${posY}`)) return false;
+        }
+        return true;
+    }
+    
+    receiveAttack(x, y)
+    {
+        if(x >= 0 && x < 10 && y >= 0 && y < 10) {
+            return { valid : false , hit : false, sunk : false };
+        }
+
+        if(this.attacks.has(`${x},${y}`)) {
+            return { valid : false, hit : false, sunk : false, reason: 'already_attacked' };
+        }
+
+        this.attacks.add(`${x},${y}`);
+        if(this.shipPositions.has(`${x},${y}`)) {
+            const ship = this.shipPositions.get(`${x},${y}`);
+            Ship.hit();
+            const sunk = ship.isSunk();
+            return { valid: true, hit: true, sunk: sunk, ship: ship };
+        }
+        this.misses.add(`${x},${y}`);
+        return { valid: true, hit: false, sunk: false };
+    }
+
+    GetMissedAttacks()
+    {
+        return Array.from(this.misses);
+    }
+
+    IsShipSunk()
+    {
+        return this.ships.every(ship => ship.ship.isSunk());
     }
 }
---
+++ src/models/player.js
@@ -0,0 +1,49 @@
+import {gameBoard} from './gameboard.js';
+
+export default class Player{
+    constructor(name,isPlayer=false){
+        this.name = name;
+        this.isPlayer = isPlayer;
+        this.gameBoard = new gameBoard();
+        this.attempts = new Set();
+    }
+
+    RandomAttack(enemyBoard)
+    {
+        if(this.isPlayer) {
+            throw new Error('Player cannot perform random attack');
+        }
+
+        const availableMoves = [];
+        for(let x = 0; x < 10; x++) {
+            for(let y = 0; y < 10; y++) {
+                if(!this.attempts.has(`${x},${y}`)) {
+                    availableMoves.push({x, y});
+                }
+            }
+        }
+        if (availableMoves.length === 0) {
+            return null; // No moves left        
+        }
+        const randomIndex = Math.floor(Math.random() * availableMoves.length);
+        const move = availableMoves[randomIndex];
+        this.attempts.add(`${move.x},${move.y}`);
+        
+        return enemyBoard.receiveAttack(move.x, move.y);
+    }
+
+    Attack(x,y, enemyBoard) {
+        const key = `${x},${y}`;
+        if(this.attempts.has(key)) {
+            return { valid: false, hit: false, sunk: false, reason: 'already_attacked' };
+        }
+
+        this.attempts.add(key);
+        return enemyBoard.receiveAttack(x, y);
+    }
+
+    resetMoves() {
+        this.attempts.clear();
+    }
+
+}
